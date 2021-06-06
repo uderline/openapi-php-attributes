63048cad0d7f201a4061e7fa5509bb82dc501db7
Joseph Martell <contact@josephmartell.com>
2021-06-06 23:35:56 +0200

    Test path method builder

---
+++ tests/PathMethodBuilderTest.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace OpenApiGenerator\Tests;
+
+use OpenApiGenerator\Attributes\Parameter;
+use OpenApiGenerator\Attributes\Property;
+use OpenApiGenerator\Attributes\PropertyItems;
+use OpenApiGenerator\Attributes\RequestBody;
+use OpenApiGenerator\Attributes\Response;
+use OpenApiGenerator\Attributes\Route;
+use OpenApiGenerator\Attributes\Schema;
+use OpenApiGenerator\PathMethodBuilder;
+use OpenApiGenerator\Types\PropertyType;
+use OpenApiGenerator\Types\RequestBodyType;
+use OpenApiGenerator\Types\SchemaType;
+use PHPUnit\Framework\TestCase;
+
+class PathMethodBuilderTest extends TestCase
+{
+
+    public function testAddResponse()
+    {
+        $response = new Response();
+
+        $builder = new PathMethodBuilder();
+        $builder->setResponse($response);
+
+        $reflection = new \ReflectionClass($builder);
+        $responseProperty = $reflection->getProperty("currentSchemaHolder");
+        $responseProperty->setAccessible(true);
+
+        self::assertEquals($response, $responseProperty->getValue($builder));
+    }
+
+    /**
+     * @requires testSetRoute
+     */
+    public function testGetRouteWithoutSavingSchemaHolder()
+    {
+        $route = new Route(Route::GET, "/path");
+
+        $builder = new PathMethodBuilder();
+
+        // Use a reflection to avoid testing PathMethodBuilder::setRoute
+        $reflection = new \ReflectionClass($builder);
+        $property = $reflection->getProperty("currentRoute");
+        $property->setAccessible(true);
+        $property->setValue($builder, $route);
+
+        self::assertEquals($route, $builder->getRoute());
+    }
+
+
+    /**
+     * The first possibility is adding a property as an object
+     *
+     * @requires testSetRoute
+     * @requires testGetRoute
+     * @requires testSetRequestBody
+     */
+    public function testAddPropertyObject()
+    {
+        $route = new Route(Route::POST, "/path");
+        $property = new Property(PropertyType::INT, "prop1");
+
+        $builder = new PathMethodBuilder();
+        $builder->setRoute($route, []);
+        $builder->setRequestBody(new RequestBody(RequestBodyType::JSON));
+        $builder->addProperty($property);
+
+        // Manually make the route
+        $schema = new Schema(SchemaType::OBJECT);
+        $schema->addProperty($property);
+        $requestBody = new RequestBody(RequestBodyType::JSON);
+        $requestBody->setSchema($schema);
+        $expectedRoute = new Route(Route::POST, "/path");
+        $expectedRoute->setRequestBody($requestBody);
+
+        self::assertEquals($expectedRoute, $builder->getRoute());
+    }
+
+    /**
+     * The other possibility is to add a property as an array
+     *
+     * @requires testGetRoute
+     */
+    public function testAddPropertyArray()
+    {
+        $route = new Route(Route::POST, "/path");
+        $property = new Property(PropertyType::ARRAY, "prop1");
+
+        $builder = new PathMethodBuilder();
+        $builder->setRoute($route, []);
+        $builder->setRequestBody(new RequestBody(RequestBodyType::JSON));
+        $builder->addProperty($property);
+
+        // Manually make the route
+        $schema = new Schema(SchemaType::ARRAY);
+        $schema->addProperty($property);
+        $requestBody = new RequestBody(RequestBodyType::JSON);
+        $requestBody->setSchema($schema);
+        $expectedRoute = new Route(Route::POST, "/path");
+        $expectedRoute->setRequestBody($requestBody);
+
+        self::assertEquals($expectedRoute, $builder->getRoute());
+    }
+
+    /**
+     * @requires testAddPropertyArray
+     * @requires testGetRoute
+     */
+    public function testAddPropertyItems()
+    {
+        $route = new Route(Route::POST, "/path");
+        $property = new Property(PropertyType::ARRAY, "prop1");
+
+        $builder = new PathMethodBuilder();
+        $builder->setRoute($route, []);
+        $builder->setRequestBody(new RequestBody(RequestBodyType::JSON));
+        $builder->addProperty($property);
+        $builder->setPropertyItems(new PropertyItems(PropertyType::INT));
+        $route = $builder->getRoute();
+
+        // Access the property items of the route to check it has been set
+        $reflection = new \ReflectionClass($route);
+        $requestBodyProperty = $reflection->getProperty("requestBody");
+        $requestBodyProperty->setAccessible(true);
+        $requestBodyPropertyValue = $requestBodyProperty->getValue($route);
+        $reflection = new \ReflectionClass($requestBodyPropertyValue);
+        $schemaProperty = $reflection->getProperty("schema");
+        $schemaProperty->setAccessible(true);
+
+        $expectedArray = [
+            "items" => [
+                "type" => PropertyType::INT,
+                "example" => ""
+            ],
+        ];
+
+        $actualArray = json_decode(json_encode($schemaProperty->getValue($requestBodyPropertyValue)), true);
+
+
+        self::assertEquals($expectedArray, $actualArray);
+    }
+
+    public function testSetRoute()
+    {
+        $route = new Route(Route::GET, "/path");
+
+        $builder = new PathMethodBuilder();
+        $builder->setRoute($route, [[new Parameter("desc")]]);
+
+        // Use a reflection to avoid testing PathMethodBuilder::getRoute
+        $reflection = new \ReflectionClass($builder);
+        $property = $reflection->getProperty("currentRoute");
+        $property->setAccessible(true);
+
+        self::assertEquals($route, $property->getValue($builder));
+    }
+
+    public function testSetRequestBody()
+    {
+        $requestBody = new RequestBody();
+
+        $builder = new PathMethodBuilder();
+        $builder->setRequestBody($requestBody);
+
+        $reflection = new \ReflectionClass($builder);
+        $schemaProperty = $reflection->getProperty("currentSchemaHolder");
+        $schemaProperty->setAccessible(true);
+
+        self::assertEquals($requestBody, $schemaProperty->getValue($builder));
+    }
+}
